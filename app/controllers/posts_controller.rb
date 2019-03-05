71d48b3aef93a77cec541521208f06b35f713070
El-Hadi <khedim.m@icloud.com>
2019-03-05 15:05:05 +0100

    First commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,74 @@
+class PostsController < ApplicationController
+    before_action :redirect_if_not_signed_in, only: [:new]
+    def show
+        @post = Post.find(params[:id])
+    end
+    def hobby
+        posts_for_branch(params[:action])
+      end
+    
+      def study
+        posts_for_branch(params[:action])
+      end
+    
+      def team
+        posts_for_branch(params[:action])
+      end
+      
+      private
+
+         def posts_for_branch(branch)
+            @categories = Category.where(branch: branch)
+            @posts = get_posts.paginate(page: params[:page])
+         end
+         
+         def get_posts
+          branch = params[:action]
+          search = params[:search]
+          category = params[:category]
+        
+          if category.blank? && search.blank?
+            posts = Post.by_branch(branch).all
+          elsif category.blank? && search.present?
+            posts = Post.by_branch(branch).search(search)
+          elsif category.present? && search.blank?
+            posts = Post.by_category(branch, category)
+          elsif category.present? && search.present?
+            posts = Post.by_category(branch, category).search(search)
+          else
+          end
+        end
+        respond_to do |format|
+          format.html
+          format.js { render partial: 'posts/posts_pagination_page' }
+        end
+
+        def get_posts
+          PostsForBranchService.new({
+            search: params[:search],
+            category: params[:category],
+            branch: params[:action]
+          }).call
+        end
+
+        def new
+          @branch = params[:branch]
+          @categories = Category.where(branch: @branch)
+          @post = Post.new
+        end
+      
+        def create
+          @post = Post.new(post_params)
+          if @post.save 
+            redirect_to post_path(@post) 
+          else
+            redirect_to root_path
+          end
+        end
+
+        def post_params
+          params.require(:post).permit(:content, :title, :category_id)
+                               .merge(user_id: current_user.id)
+        end
+
+end
